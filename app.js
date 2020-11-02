f04892c47a31019fde8f3d0e2c511bcd1f4cd15d
jopakka <jopakkamies@gmail.com>
2020-11-02 21:00:17 +0200

    cookies set and delete work

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 'use strict';
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+// Set cookie
+app.get('/setCookie/:clr', (req, res) => {
+  res.cookie('color', req.params.clr);
+  res.send('Cookie set');
+});
+
+// Delete cookie
+app.get('/deleteCookie', (req, res) => {
+  res.clearCookie('color');
+  res.send('Cookie deleted');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
